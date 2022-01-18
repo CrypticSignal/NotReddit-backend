483e3f0f3ff14319595d227717fd0a7f44376645
CrypticSignal <hshafiq@hotmail.co.uk>
2022-01-18 15:34:29 +0000

    Add tests for a request made to an invalid endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,6 +7,14 @@ const testData = require("../db/data/test-data/index.js");
 beforeEach(() => seed(testData));
 afterAll(() => db.end());
 
+describe("Request made to an invalid endpoint", () => {
+  it("Respond with a status of 404 and a message of 'Invalid endpoint", async () => {
+    const response = await request(app).get("/invalid-endpoint");
+    expect(response.status).toBe(404);
+    expect(response.body).toEqual({ msg: "Invalid endpoint" });
+  });
+});
+
 describe("/api/topics", () => {
   it("Responds with a status of 200", async () => {
     const response = await request(app).get("/api/topics");
