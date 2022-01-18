0204d2523f3c6567f980e570c001fe327f6f996d
CrypticSignal <hshafiq@hotmail.co.uk>
2022-01-18 15:20:29 +0000

    Create tests for the /api/topics endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,29 @@
-const db = require('../db/connection.js');
-const testData = require('../db/data/test-data/index.js');
-const { seed } = require('../db/seeds/seed.js');
+const app = require("../app");
+const db = require("../db/connection.js");
+const request = require("supertest");
+const seed = require("../db/seeds/seed.js");
+const testData = require("../db/data/test-data/index.js");
 
 beforeEach(() => seed(testData));
 afterAll(() => db.end());
+
+describe("/api/topics", () => {
+  it("Responds with a status of 200", async () => {
+    const response = await request(app).get("/api/topics");
+    expect(response.status).toBe(200);
+  });
+  it("Returns an object containing an array of topics in the correct format.", async () => {
+    const response = await request(app).get("/api/topics");
+    const topicsObject = response.body;
+    expect(topicsObject).toBeInstanceOf(Object);
+    expect(topicsObject.topics).toBeInstanceOf(Array);
+    topicsObject.topics.forEach((topic) => {
+      expect(topic).toEqual(
+        expect.objectContaining({
+          slug: expect.any(String),
+          description: expect.any(String),
+        })
+      );
+    });
+  });
+});
