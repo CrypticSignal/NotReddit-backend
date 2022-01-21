8bc92f860eb55d8bb5f7ca3d72cbc69dea5bf414
CrypticSignal <hshafiq@hotmail.co.uk>
2022-01-21 16:08:20 +0000

    Expect specific values in the object

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -45,18 +45,17 @@ describe("GET /api/article/:article_id", () => {
   it("Returns an object containing the article in the correct format.", async () => {
     const { body } = await request(app).get("/api/articles/2");
     expect(body).toBeInstanceOf(Object);
-    expect(body.article).toBeInstanceOf(Array);
-    expect(body.article[0]).toBeInstanceOf(Object);
-    expect(body.article[0]).toEqual(
+    expect(body.article).toBeInstanceOf(Object);
+    expect(body.article).toEqual(
       expect.objectContaining({
-        article_id: expect.any(Number),
-        title: expect.any(String),
-        body: expect.any(String),
-        votes: expect.any(Number),
-        topic: expect.any(String),
-        author: expect.any(String),
+        article_id: 2,
+        title: "Sony Vaio; or, The Laptop",
+        body: "Call me Mitchell. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would buy a laptop about a little and see the codey part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off—then, I account it high time to get to coding as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the laptop. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the the Vaio with me.",
+        votes: 0,
+        topic: "mitch",
+        author: "icellusedkars",
         created_at: "2020-10-16T05:03:00.000Z",
-        comment_count: expect.any(Number),
+        comment_count: 0,
       })
     );
   });
